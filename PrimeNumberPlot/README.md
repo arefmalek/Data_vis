86afe19fb952d6561619fe626bde674777193fcf
arefmalek <malek.aref1@gmail.com>
2020-10-30 22:26:28 -0700

    tryna do something

---
+++ PrimeNumberPlot/README.md
@@ -0,0 +1,3 @@
+# Prime Number Plot
+
+I watched a 3blue1brown video on this and knew I had to try it
---
+++ PrimeNumberPlot/prime_num.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def polar(primes):
+    plt.polar(primes, primes, "ro")
+    plt.ylabel('primes')
+    plt.show()
+
+def sieve(number):
+    answer = []
+    plist = [True] * number
+    for i in range(2, number):
+        if plist[i]:
+            answer.append(i)
+            for j in range(i*i, number, i):
+                plist[j] = False
+    
+    polar(answer)
+
+def regular(number):
+    polar(np.arange(number))
+
+
+n = 1_000
+
+this = "yes"
+this += str(n)
+print(this)
