646bc57be4ad9e8c6691a1c308262821438ea03d
arefmalek <malek.aref1@gmail.com>
2020-11-01 15:15:21 -1000

    I added the pictures of cooler ones

--- PrimeNumberPlot/README.md
+++ PrimeNumberPlot/README.md
@@ -1,3 +1,7 @@
 # Prime Number Plot
 
 I watched a 3blue1brown video on this and knew I had to try it
+#
+![alt text](PrimeNumberPlot/images/Primes_under_100.PNG)
+
+![another](PrimeNumberPlot/images/Primes_under_100000.png)
--- PrimeNumberPlot/prime_num.py
+++ PrimeNumberPlot/prime_num.py
@@ -1,25 +1,47 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def polar(primes):
-    plt.polar(primes, primes, "ro")
-    plt.ylabel('primes')
+import math
+
+def polar(primes, limit):
+    
+    title = "Primes_under_{0}".format(limit)
+    print(title)
+
+    plt.title("Primes under {0}".format(limit))
+
+    plt.subplot(1, 2, 1, projection = "polar")
+
+    plt.polar(np.arange(limit), np.arange(limit), "g.", 
+    markersize = 5/math.log10(limit))
+    plt.xlabel("Regular")
+    
+    plt.subplot(1, 2, 2, projection = "polar")
+    plt.polar(primes, primes, "r.", 
+    markersize = 5/math.log10(limit))
+    plt.xlabel('Primes')
+   
     plt.show()
 
-def sieve(number):
+    # filename = "PrimeNumberPlot/primes_under_{total}.jpg".format(total = n)
+    # plt.savefig(filename)   
+
+
+
+
+
+def sieve(limit):
     answer = []
-    plist = [True] * number
-    for i in range(2, number):
+    plist = [True] * limit
+    for i in range(2, limit):
         if plist[i]:
             answer.append(i)
-            for j in range(i*i, number, i):
+            for j in range(i*i, limit, i):
                 plist[j] = False
     
-    polar(answer)
-
-def regular(number):
-    polar(np.arange(number))
+    polar(answer, limit)
 
 
-n = 1_000
 
+n = 100_000
+sieve(n)
