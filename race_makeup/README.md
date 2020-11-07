859da8b8c6a5da67f5dbc618ca7e3343066f702f
arefmalek <malek.aref1@gmail.com>
2020-11-07 01:12:09 -0800

    burnt but good day :)

---
+++ race_makeup/README.md
@@ -0,0 +1,6 @@
+# Electoral College
+
+With the election passing by this week, I felt I had an opportunity to visit a concept that drove the nation mad 4 years ago - how could someone be chosen by an additional 2 million Americans and still lose? I pondered about who the electoral college really serves, and wanted to get an actual representation of just that
+
+## Graphs
+![Version1]("images/version_1.png")
---
+++ race_makeup/data_parse.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import json
+
+ecv = pd.read_csv("https://raw.githubusercontent.com/PitchInteractiveInc/tilegrams/master/data/us/electoral-college-votes-by-state.csv", header=None)
+gdp = pd.read_csv("https://raw.githubusercontent.com/PitchInteractiveInc/tilegrams/master/data/us/gdp-by-state.csv", header=None)
+pop = pd.read_csv("https://raw.githubusercontent.com/PitchInteractiveInc/tilegrams/master/data/us/population-by-state.csv", header=None)
+
+nhpop = pd.read_csv("race_makeup/data/rmf.txt", sep="\t", header = None)
+
+print(nhpop.head())
+nhpop[2] = nhpop[2].str.rstrip(r'\d%').astype(float)
+
+with open("race_makeup/data/statekey.json") as f:
+    info = json.load(f)
+
+states = []
+for value in info.values():
+    states.append(value["name_short"])
+
+# print(states)
+# print(pop[1])
+
+
+df = pd.DataFrame({
+    "State": states,
+    "E.C. Votes": ecv[1],
+    "GDP": gdp[1],
+    "Population": pop[1],
+    "N.H.W Percentage": nhpop[2]
+})
+
+
+
+df.to_csv("race_makeup/data/state_data", index = False)
